f2a971664c6b67e3a349cf92f5311e03da20cb97
Piyush762051 <khandarepiyush2@gmail.com>
2024-12-03 14:44:32 +0530

    PoJo class Entity

--- CrudOperationGit/src/main/java/com/cjc/main/model/Student.java
+++ CrudOperationGit/src/main/java/com/cjc/main/model/Student.java
@@ -1,6 +1,21 @@
 package com.cjc.main.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class Student
 {
+	@Id
+	private int studentRollno;
+	private String studentName;
+	private String studentMobno;
+	private String studentAge;
 	
 }

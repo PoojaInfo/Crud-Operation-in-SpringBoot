85556c737ac187ec9ea460e96a1992823c7ca9c2
vaibhavkadam0698 <vking6007@gmail.com>
2024-12-03 16:10:45 +0530

    Update StudentControlller.java

--- CrudOperationGit/src/main/java/com/cjc/main/controller/StudentControlller.java
+++ CrudOperationGit/src/main/java/com/cjc/main/controller/StudentControlller.java
@@ -1,25 +1,18 @@
 package com.cjc.main.controller;
 
 
-<<<<<<< HEAD
 import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-
-=======
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
->>>>>>> branch 'main' of https://github.com/PoojaInfo/Crud-Operation-in-SpringBoot.git
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cjc.main.model.Student;
@@ -29,23 +22,18 @@ import com.cjc.main.serviceI.StudentServicesI;
 public class StudentControlller 
 {
 	@Autowired
-<<<<<<< HEAD
-	StudentServicesI studentServicesI;
+	StudentServicesI ssi;
+	
+	private static final Logger LOG=LoggerFactory.getLogger(StudentControlller.class);
 	
 	@GetMapping("/getAllData")
    public ResponseEntity<List<Student>> ongetData()
  {
-		List<Student> al=studentServicesI.getstudent();
+		List<Student> al=ssi.getstudent();
 	return new ResponseEntity<List<Student>>(al,HttpStatus.OK);
 	 
  }
- 
 	
-	
-=======
-	StudentServicesI ssi;
-	
-	private static final Logger LOG=LoggerFactory.getLogger(StudentControlller.class);
 	
 	@PostMapping("/postData")
 	public ResponseEntity<Student> onsaveData(@RequestBody Student student)
@@ -61,5 +49,5 @@ public class StudentControlller
 					ssi.deleteData(id);
 					return new ResponseEntity<String>("Id delete",HttpStatus.OK);
 	}
->>>>>>> branch 'main' of https://github.com/PoojaInfo/Crud-Operation-in-SpringBoot.git
+
 }
